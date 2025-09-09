7cb7404c61db03add0f03504b4a7f315d0b1b153
Roy Ren <jiahuaren135@gmail.com>
2025-09-09 13:36:17 -0700

    fix cookie domain issue

--- backend/src/main/java/com/app/todoapp/controller/DebugController.java
+++ backend/src/main/java/com/app/todoapp/controller/DebugController.java
@@ -9,17 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 
-@RestController
+// @RestController
 
 public class DebugController {
 
-    @GetMapping("/api/debug/cookies")
-    public ResponseEntity<String> debugCookies(HttpServletRequest request) {
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
-            Arrays.stream(cookies).forEach(c -> System.out.println(c.getName() + "=" + c.getValue()));
-        }
-        return ResponseEntity.ok("Cookies printed to console");
-    }
+    // @GetMapping("/api/debug/cookies")
+    // public ResponseEntity<String> debugCookies(HttpServletRequest request) {
+    // Cookie[] cookies = request.getCookies();
+    // if (cookies != null) {
+    // Arrays.stream(cookies).forEach(c -> System.out.println(c.getName() + "=" +
+    // c.getValue()));
+    // }
+    // return ResponseEntity.ok("Cookies printed to console");
+    // }
 
 }
