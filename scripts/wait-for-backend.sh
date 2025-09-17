b6613d3025fff0ae19aa1a5140e18774a51e45db
Roy Ren <jiahuaren135@gmail.com>
2025-09-16 23:29:03 -0700

    try to make nextjs explicitly localhost

--- scripts/wait-for-backend.sh
+++ scripts/wait-for-backend.sh
@@ -6,7 +6,7 @@ java -jar /app/app.jar --server.port=8080 --server.address=127.0.0.1 --spring.pr
 
 # Start frontend
 cd /app/frontend
-HOST=127.0.0.1 PORT=3000 npm start &
+npm start -- -H 127.0.0.1 -p 3000 &
 
 until curl -sf http://127.0.0.1:8080/actuator/health; do
   echo "Waiting for backend..."
